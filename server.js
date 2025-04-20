f94fd05f85ba48962667c70a6c8ce82952c45701
sathana-v <sathanav.19cse@kongu.edu>
2025-04-20 16:10:14 +0530

    updated file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get('/', (req,res) => {
-    res.send("welcome");
+    res.send("welcome user");
 })
 app.listen(3000, function() {
     console.log("app lsiterning port 3000")
